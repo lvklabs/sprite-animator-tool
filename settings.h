5412003488ab786cbcf29209cdc92f1e181d9ffc
Andres Pagliano <paglian@gmail.com>
2009-11-05 23:53:43 -0300

    Improved "open recent" menu. Now remember the last 10 files that were opened. There are minor issues like store duplicated entries.

---
+++ settings.h
@@ -0,0 +1,10 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#define APP_NAME                "Lvk Sprite Animation Tool"
+#define APP_VERSION             "0.1"
+
+#define MAX_RECENT_FILES        10
+#define KEY_RECENT_FILE         "RecentFiles/filename"
+
+#endif // SETTINGS_H
