c70e4ae8c8fe029a775574da9808b06119830221
Andres Pagliano <paglian@gmail.com>
2009-12-28 22:50:30 -0300

    Updated count-lines-of-code.sh

--- util/count-lines-of-code.sh
+++ util/count-lines-of-code.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-rm -f moc_*.cpp
+echo "Counting lines of C++ source code in" `pwd` 
+rm -f moc_*.cpp ui_mainwindow.hi qrc_res.cpp
 echo "Total lines               :" `cat *.cpp *.h | wc -l`
 echo "Total lines without spaces:" `cat *.cpp *.h | grep -v -e "^\s*$" | wc -l`
